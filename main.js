7aa46805386254ddd268de6379c5c915bb19a18a
Salma Zainab <104749072+xsalmazainabx@users.noreply.github.com>
2024-04-08 21:58:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const display =document.querySelector(".display");
+const buttons =document.querySelectorAll("button");
+const specialChar =["%","*","/","-","+","="];
+let output="";
+
+const calculate=(btnValue) =>{
+    display.focus();
+    if (btnValue ==="="&& output!=="") {
+        output=eval(output.replace("%","/100"));
+        
+    } else if (btnValue==="AC") {
+        output="";
+        
+    } else if (btnValue==="DEL") {
+        output=output.toString().slice(0,-1);
+        
+    } else{
+    if (output ===""&&  specialChar.includes(btnValue)) return; 
+        output+=btnValue;
+            
+        }
+        display.value=output;
+    };
+
+    buttons.forEach((button) => {
+        button.addEventListener("click",(e) => calculate(e.target.dataset.value));
+
+    });
